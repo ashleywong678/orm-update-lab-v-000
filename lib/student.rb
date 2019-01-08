3ca3c787990e97879bb5f43c5517dcab1d527e32
Ashley Wong <ashwong04@gmail.com>
2019-01-08 01:18:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,8 @@ class Student
   end
   
   def self.find_by_name(name)
-    
+    row =DB[:conn].execute("SELECT * FROM students WHERE name = ?", name)
+    self.new_from_db(row[0])
   end
   
 end
